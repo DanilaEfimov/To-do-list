9d2c58b5504cfeb44892a8aaeec6f456481290f3
DanilaEfimov <efimovdanila.de@gmail.com>
2024-09-09 22:36:36 +0400

    Update PushButton.cpp

--- Classes/PushButton.cpp
+++ Classes/PushButton.cpp
@@ -1,9 +1,9 @@
-#include "PushButton.h"
+#include "TriggerButton.h"
 
-const color PushButton::on = {0, 255, 0};
-const color PushButton::off = {255, 0, 0};
+const color TriggerButton::on = {0, 255, 0};
+const color TriggerButton::off = {255, 0, 0};
 
-PushButton::PushButton(int x, int y, int xPos, int yPos) {
+TriggerButton::TriggerButton(int x, int y, int xPos, int yPos) {
 	this->size = new vec2i{ abs(x), abs(y) };
 	this->triggered = new bool{ false }; // strange maybe?
 	this->buttonRect = new rect{ static_cast<sf::Vector2f>(*this->size) };
@@ -16,13 +16,13 @@ PushButton::PushButton(int x, int y, int xPos, int yPos) {
 	}
 }
 
-PushButton::~PushButton() {
+TriggerButton::~TriggerButton() {
 	delete this->size;
 	delete this->triggered;
 	delete this->buttonRect;
 }
 
-void PushButton::triggering() {
+void TriggerButton::triggering() {
 	*(this->triggered) = !(*triggered);
 	if (*(this->triggered)) {
 		this->buttonRect->setFillColor(on);
@@ -32,6 +32,6 @@ void PushButton::triggering() {
 	}
 }
 
-void PushButton::draw(sf::RenderWindow* pWin) {
+void TriggerButton::draw(sf::RenderWindow* pWin) {
 	pWin->draw(*this->buttonRect);
 }
