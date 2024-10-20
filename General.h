0909e3db3186437f60c1d51b965d07b4af6f6eb0
DanilaEfimov <efimovdanila.de@gmail.com>
2024-10-20 23:22:34 +0400

    Update General.h

--- General.h
+++ General.h
@@ -4,7 +4,7 @@
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 1000
 #define BACK_FILL sf::Color{255, 255, 100}
-#define EPSILONE 0.7f					// it width of note in windows
+#define EPSILONE 0.7f					// it's width of note in windows
 #define DELTA 10						// count of notes in window
 #define TEXT_NOTE_HEIGHT_RATIO 0.7f		// text height / note height
 #define TEXT_SIZE static_cast<int>((WINDOW_HEIGHT / DELTA) * TEXT_NOTE_HEIGHT_RATIO) // in pixels
